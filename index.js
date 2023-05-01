851ba1fbce2346293d4646b0ddfe4c9f3c80f9bc
Ankit <ankit26kushwa@gmail.comm>
2023-05-01 13:17:01 +0530

    repo setup

---
+++ index.js
@@ -0,0 +1,10 @@
+
+import { DateTime } from "./node_modules/luxon/build/es6/luxon.js";
+
+// const greeting = 'Hello, world!';
+
+function sayHello() {
+  console.log(DateTime.DATE_FULL);
+}
+
+sayHello();
