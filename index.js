41819def51b32e99ce23331107742d3af6d03e95
Ankit <ankit26kushwa@gmail.comm>
2023-05-06 16:51:16 +0530

    Fix fetch item

--- index.js
+++ index.js
@@ -4,20 +4,8 @@ import RemoveBook from './modules/removeBook.js';
 import NavLinks from './modules/navLinks.js';
 import Time from './modules/Time.js';
 
-// check booklist in localStorage
-
-let BookList;
-if (localStorage.getItem('BookList')) {
-  const toParse = JSON.parse(localStorage.getItem('BookList'));
-  BookList = toParse;
-} else {
-  BookList = [];
-}
-
 NavLinks();
 Time();
-DisplayBook(BookList);
-AddBook(BookList);
-DisplayBook(BookList);
-RemoveBook(BookList);
-DisplayBook(BookList);
+DisplayBook();
+AddBook();
+RemoveBook();
